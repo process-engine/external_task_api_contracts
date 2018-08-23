295286c844c1a8fdae1c0333760629aa2daad996
Thomas Konrad <thomas.konrad@5minds.de>
2018-08-23 11:51:46 +0200

    Added dotnet files

---
+++ src/IExternalTaskAPIFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessEngine.ExternalTaskAPI.Contracts
+{
+    /// <summary>
+    /// Interface for an factory for creating ExternalTaskAPI services.
+    /// </summary>
+    public interface IExternalTaskAPIFactory
+    {
+        /// <summary>
+        /// Factory method for creating an ExternalTaskAPIService.
+        /// </summary>
+        /// <returns>The ExternalTaskAPIService.</returns>
+        /// <param name="processEngineAPIBaseUrl">The base URL of the process engine.</param>
+        /// <param name="userAgent">The user agent to put in the HTTP header.</param>
+        IExternalTaskAPIService CreateExternalTaskAPIService(string processEngineAPIBaseUrl, string userAgent);
+    }
+}
