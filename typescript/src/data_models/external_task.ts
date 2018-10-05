0f3cb8af6bfce6494af38d5ddbe7c52deaa92583
ElRaptorus <christian.werner@5minds.de>
2018-10-05 10:41:31 +0200

    :recycle: Make ExternalTask object immutable

--- typescript/src/data_models/external_task.ts
+++ typescript/src/data_models/external_task.ts
@@ -1,36 +1,113 @@
+import * as moment from 'moment';
+
 /**
- * An ExternalTask, the ProcessEngine is providing for external services to
- * process.
+ * Describes an ExternalTask that the ProcessEngine has delegated to an
+ * ExternalTask worker for processing.
  */
 export class ExternalTask {
+
+  private readonly _id: string;
+  private readonly _workerId: string;
+  private readonly _topic: string;
+  private readonly _flowNodeInstanceId: string;
+  private readonly _correlationId: string;
+  private readonly _processInstanceId: string;
+  private readonly _lockExpirationTime?: Date;
+  private readonly _payload: any;
+
+  constructor(id: string,
+              workerId: string,
+              topic: string,
+              flowNodeInstanceId: string,
+              correlationId: string,
+              processInstanceId: string,
+              lockExpirationTime: Date,
+              payload: any) {
+  this._id = id;
+  this._workerId = workerId;
+  this._topic = topic;
+  this._flowNodeInstanceId = flowNodeInstanceId;
+  this._correlationId = correlationId;
+  this._processInstanceId = processInstanceId;
+  this._lockExpirationTime = lockExpirationTime;
+  this._payload = payload;
+  }
+
   /**
-   * The process variables and token data to use by the ExternalTask service.
+   * The external tasks ID.
    */
-  public payload: any;
+  public get id(): string {
+    return this._id;
+  }
+
   /**
-   * The ID of the ExternalTask.
+   * The ID of the worker that has most recently locked the ExternalTask
+   * for processing.
    */
-  public id: string;
+  public get workerId(): string {
+    return this._workerId;
+  }
+
   /**
-   * The FlowNodeIstance ID in the ProcessModel.
+   * The topic to which the ExternalTask belongs.
    */
-  public flowNodeInstanceId: string;
+  public get topic(): string {
+    return this._topic;
+  }
+
   /**
-   * The ID of the Correlation the ProcessInstance belongs to.
+   * The ID of the FlowNodeInstance that contains the definition
+   * for the ExternalTask.
    */
-  public correlationId: string;
+  public get flowNodeInstanceId(): string {
+    return this._flowNodeInstanceId;
+  }
+
   /**
-   * The ProcessInstanceId.
+   * The ID of the Correlation that contains the FlowNodeInstance with the
+   * ExternalTask's definition.
    */
-  public processInstanceId: string;
+  public get correlationId(): string {
+    return this._correlationId;
+  }
+
   /**
-   * The worker ID of the ExternalTask.
-   * The Processengine locks the task for this worker to process.
+   * The ID of the ProcessInstance that contains the FlowNodeInstance with the
+   * ExternalTask's definition.
    */
-  public workerId: string;
+  public get processInstanceId(): string {
+    return this._processInstanceId;
+  }
+
   /**
    * The lock expiration time. On expiration, the task is released to
    * be processed by other workers.
+   * If not set, the ExternalTask is not locked.
    */
-  public lockExpirationTime: Date;
+  public get lockExpirationTime(): Date {
+    return this._lockExpirationTime;
+  }
+
+  /**
+   * The payload containing all relevant data the worker needs to execute the
+   * ExternalTask.
+   */
+  public get payload(): any {
+    return this._payload;
+  }
+
+  /**
+   * Determines whether the ExternalTask is currently locked or not.
+   * A finished ExternalTask is always locked.
+   */
+  public get isLocked(): boolean {
+    if (!this.lockExpirationTime) {
+      return false;
+    }
+
+    const lockExpirationAsMoment: moment.Moment = moment(this.lockExpirationTime);
+    const now: moment.Moment = moment();
+
+    return lockExpirationAsMoment.isAfter(now);
+  }
 }
