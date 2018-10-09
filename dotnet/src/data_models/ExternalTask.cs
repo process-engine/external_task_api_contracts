46e7b879a1a52635d659418d0e8d63be31e08754
ElRaptorus <christian.werner@5minds.de>
2018-10-09 11:54:35 +0200

    :recycle: Import changes from .ts to .NET implementation

--- dotnet/src/data_models/ExternalTask.cs
+++ dotnet/src/data_models/ExternalTask.cs
@@ -1,63 +1,120 @@
 ﻿namespace ProcessEngine.ExternalTaskAPI.Contracts
 {
+    using System;
+
     /// <summary>
-    /// An external task the engine is providing for external services to process.
+    /// Describes an ExternalTask that the ProcessEngine has delegated to an
+    /// ExternalTask worker for processing.
     /// </summary>
     public class ExternalTask<TPayload>
     {
         /// <summary>
-        /// Constructor
+        /// Constructor.
         /// </summary>
-        /// <param name="payload">The payload for the external tasks.</param>
+        /// <param name="payload">The payload for the ExternalTask.</param>
         public ExternalTask(TPayload payload)
         {
             Payload = payload;
         }
 
         /// <summary>
-        /// The process variables and token data to use and process by the external task
+        /// The Id of the ExternalTask.
+        /// </summary>
+        public string Id;
+
+        /// <summary>
+        /// The ID of the worker that has most recently locked the ExternalTask
+        /// for processing.
+        /// </summary>
+        public string WorkerId;
+
+        /// <summary>
+        /// Contains the topic this ExternalTask is associated with.
+        /// </summary>
+        public string Topic;
+
+        /// <summary>
+        /// The ID of the Activity that contains the definition of the
+        /// ExternalTask.
+        /// </summary>
+        public string ActivityId;
+
+        /// <summary>
+        /// The ID of the correlation that contains the activity with the
+        /// ExternalTask's definition.
+        /// </summary>
+        public string CorrelationId;
+
+        /// <summary>
+        /// The ID of the ProcessInstance that contains the activity with the
+        /// ExternalTask's definition.
+        /// </summary>
+        public string ProcessInstanceId;
+
+        /// <summary>
+        /// The payload containing all relevant data the worker needs to execute
+        /// the ExternalTask.
         /// </summary>
         /// <value>The payload.</value>
         public TPayload Payload { get; }
 
         /// <summary>
-        /// The Id of the external task.
+        /// The lock expiration time. On expiration, the task is released to
+        /// be processed by other workers.
+        /// If not set, the ExternalTask is not locked.
         /// </summary>
-        public string Id;
+        public Nullable<DateTime> LockExpirationTime;
 
         /// <summary>
-        /// The activity Id in the process model.
+        /// Uses the defined LockExpirationTime to determine if the ExternalTask
+        /// is currently locked.
         /// </summary>
-        public string ActivityId;
+        /// <value></value>
+        public bool IsLocked {
+          get {
+            if (this.LockExpirationTime == null) {
+              return false;
+            }
+
+            var now = DateTime.Now;
+
+            return this.LockExpirationTime > now;
+          }
+        }
 
         /// <summary>
-        /// The correlation Id in which the process is running.
+        /// Describes the state the ExternalTask is currently in.
+        /// Default state is "pending".
         /// </summary>
-        public string CorrelationId;
+        public ExternalTaskState State = ExternalTaskState.pending;
 
         /// <summary>
-        /// The process instance Id.
+        /// Contains the Date and Time at wich the ExternalTask was finished.
+        /// If not set, the ExternalTask has not been finished yet.
         /// </summary>
-        public string ProcessInstanceId;
+        public Nullable<DateTime> FinishedAt;
 
         /// <summary>
-        /// The worker Id of the external task. The process engine locks the task for this worker for processing.
+        /// If the ExternalTask was finished successfully, this will contain the
+        /// ExteralTask's result.
         /// </summary>
-        public string WorkerId;
+        public object Result;
 
         /// <summary>
-        /// The lock expiration time after that the task is released to be processed by other services.
-        /// </summary>
-        public string LockExpirationTime;
-
-        /// <summary>
-        /// The full error message submitted with the latest reported failure executing this task.
+        /// If the ExternalTask was finished with an error, this will contain
+        /// the errors message.
         /// </summary>
         public string ErrorMessage;
 
         /// <summary>
-        /// The error details submitted with the latest reported failure executing this task.
+        /// /// If the ExternalTask was finished with an error, this will contain
+        /// the errors details.
         /// </summary>
         public string ErrorDetails;
+
+        /// <summary>
+        /// The Date and Time at which the ExternalTask was created.
+        /// </summary>
+        public DateTime CreatedAt;
     }
 }
