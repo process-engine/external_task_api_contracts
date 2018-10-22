5c0eba2c19d20476653a80bbae3ab394bb018604
Robin <robin.lenz@5minds.de>
2018-10-22 16:09:56 +0200

    :sparkles: dotnet contracts for external task worker added

---
+++ dotnet/src/ExternalTaskFinished.cs
@@ -0,0 +1,48 @@
+namespace ProcessEngine.ExternalTaskAPI.Contracts
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Foundation.IAM.Contracts;
+
+    /// <summary>
+    /// Contains the result set for a successfully executed ExternalTask.
+    /// </summary>
+    public class ExternalTaskFinished<TPayload> : IExternalTaskResult
+    {
+        private readonly string externalTaskId;
+        private readonly TPayload result;
+
+        /// <summary>
+        /// Creates a new Instace of ExternalTaskFinished
+        /// </summary>
+        /// <param name="externalTaskId">
+        /// Id of the ExternalTask
+        /// </param>
+        /// <param name="result">
+        /// Result of executing ExternalTask
+        /// </param>
+        public ExternalTaskFinished(string externalTaskId, TPayload result)
+        {
+            this.externalTaskId = externalTaskId;
+            this.result = result;
+        }
+
+        /// <summary>
+        /// Sends the ExternalTasks result to the ExternalTaskAPI, using the given 'IExternalTaskAPI' instance.
+        /// </summary>
+        /// <param name="externalTaskAPI">
+        /// The ExternalTaskAPI instance that should process the ExternalTasks result.
+        /// </param>
+        /// <param name="identity">
+        /// The identity to use for sending the ExternalTasks result.
+        /// </param>
+        /// <param name="workerId">
+        /// Id of the Worker wich handled this tasks.
+        /// </param>
+        public async Task SendToExternalTaskApi(IExternalTaskAPI externalTaskAPI, IIdentity identity, string workerId)
+        {
+            await externalTaskAPI.FinishExternalTask<TPayload>(identity, workerId, this.externalTaskId, this.result);
+        }
+    }
+}
---
+++ dotnet/src/IExternalTaskWorker.cs
@@ -0,0 +1,51 @@
+namespace ProcessEngine.ExternalTaskAPI.Contracts
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Foundation.IAM.Contracts;
+
+
+    /// <summary>
+    /// Definition of the HandleExternalTask Callback.
+    /// </summary>
+    public delegate IExternalTaskResult HandleExternalTaskAction<TPayload>(ExternalTask<TPayload> externalTask);
+
+    /// <summary>
+    /// Periodically fetches, locks and processes ExternalTasks for a given topic.
+    /// </summary>
+    public interface IExternalTaskWorker
+    {
+        /// <summary>
+        /// Id of worker
+        /// </summary>
+        string WorkerId { get; }
+
+        /// <summary>
+        /// Periodically fetches, locks and processes available ExternalTasks with a given topic,
+        /// using the given callback as a processing function.
+        /// </summary>
+        /// <param name="identity">
+        /// The identity to use for fetching and processing ExternalTasks.
+        /// </param>
+        /// <param name="topic">
+        /// The topic by which to look for and process ExternalTasks.
+        /// </param>
+        /// <param name="maxTasks">
+        /// max. ExternalTasks to fetch.
+        /// </param>
+        /// <param name="longpollingTimeout">
+        /// Longpolling Timeout in ms.
+        /// </param>
+        /// <param name="handleAction">
+        /// The function for processing the ExternalTasks.
+        /// </param>
+        Task WaitForHandle<TPayload>(
+            IIdentity identity,
+            string topic,
+            int maxTasks,
+            int longpollingTimeout,
+            HandleExternalTaskAction<TPayload> handleAction
+          );
+    }
+}
