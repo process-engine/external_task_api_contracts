ef5a52dc0b6b6fb7a27b9598216d10ecc96dccf6
Robin <robin.lenz@5minds.de>
2018-10-22 14:16:28 +0200

    :recycle: changes reguested in pr

--- typescript/src/external_task_finished.ts
+++ typescript/src/external_task_finished.ts
@@ -18,9 +18,9 @@ export class ExternalTaskFinished<TResult> implements IExternalTaskResult {
    * Sends the ExternalTasks result to the ExternalTaskAPI, using the given 'IExternalTaskAPI' instance.
    *
    * @async
-   * @param externalTaskApi    Service for handling external Task.
-   * @param identity           IIdentity to fetch Tasks for.
-   * @param workerId           Id of the Worker with handled this tasks.
+   * @param externalTaskApi    The ExternalTaskAPI instance that should process the ExternalTasks result.
+   * @param identity           The identity to use for sending the ExternalTasks result.
+   * @param workerId           Id of the Worker wich handled this tasks.
    */
   public async sendToExternalTaskApi(externalTaskApi: IExternalTaskApi, identity: IIdentity, workerId: string): Promise<void> {
     await externalTaskApi.finishExternalTask<TResult>(identity, workerId, this._externalTaskId, this._result);
